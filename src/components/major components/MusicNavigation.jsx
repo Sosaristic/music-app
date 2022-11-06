2559f7eaad1968c1b3d24e9a8d862a83338f54d0
Sosaristic <sundayomena9@gmail.com>
2022-11-06 20:03:25 +0100

    Update: added music navigation icons

--- src/components/major components/MusicNavigation.jsx
+++ src/components/major components/MusicNavigation.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import { Box } from "@mui/system";
 import { Avatar, Typography } from "@mui/material";
-import LibraryMusicIcon from "@mui/icons-material/LibraryMusic";
+
+import BarChartIcon from '@mui/icons-material/BarChart'
+import EmojiPeopleIcon from '@mui/icons-material/EmojiPeople';
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import DiscFullIcon from '@mui/icons-material/DiscFull';
+import WhatshotIcon from '@mui/icons-material/Whatshot';
+import AlbumIcon from '@mui/icons-material/Album';
+import WifiTetheringIcon from '@mui/icons-material/WifiTethering';
+import ChurchIcon from '@mui/icons-material/Church';
+
 
 export default function MusicNavigation() {
   const Card = ({ title, icon }) => {
     return (
-      <Box mr={3}>
+      <Box mr={3.5} sx={{display: "flex", flexDirection: "column", alignItems: "center"}}>
         <Avatar sx={{ bgcolor: "action.active", color: "primary.main" }}>
-          <LibraryMusicIcon />
+          {icon}
         </Avatar>
         <Typography>{title}</Typography>
       </Box>
@@ -25,14 +34,14 @@ export default function MusicNavigation() {
         scrollbarWidth: "none",
       }}
     >
-      <Card title={"Charts"} />
-      <Card title={"Artists"} />
-      <Card title={"Playlists"} />
-      <Card title={"Genres"} />
-      <Card title={"Trending"} />
-      <Card title={"New"} />
-      <Card title={"Gospel"} />
-      <Card title={"Podcasts"} />
+      <Card title={"Charts"} icon= {<BarChartIcon/>}/>
+      <Card title={"Artists"} icon={<EmojiPeopleIcon/>}/>
+      <Card title={"Playlists"}  icon={<MusicNoteIcon/>}/>
+      <Card title={"Genres"} icon={<DiscFullIcon/>}/>
+      <Card title={"Trending"} icon={<WhatshotIcon/>}/>
+      <Card title={"New"} icon={<AlbumIcon/>}/>
+      <Card title={"Gospel"} icon={<ChurchIcon/>}/>
+      <Card title={"Podcasts"} icon={<WifiTetheringIcon/>}/>
     </Box>
   );
 }
